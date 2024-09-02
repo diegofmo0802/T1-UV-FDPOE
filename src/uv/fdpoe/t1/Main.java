d0d5b0ca4a56ce42d381993a4a9b52ff2e6f74b7
diegofmo0802 <diegofmo0802@gmail.com>
2024-09-01 21:20:08 -0500

    main menu added and about us added

--- src/uv/fdpoe/t1/Main.java
+++ src/uv/fdpoe/t1/Main.java
@@ -1,11 +1,32 @@
 package uv.fdpoe.t1;
-import javax.swing.JOptionPane;
+import java.util.ArrayList;
 public class Main {
+    public ArrayList<People> peoples;
+    public void mainMenu() {
+        boolean running = true;
+        String[] options = {
+            "entity managment",
+            "list menu",
+            "statistics",
+            "about us",
+            "close app"
+        };
+        while (running) {
+            String selected = Utilities.selectWindow("Main menu", "select an option", options);
+            Utilities.logConcat("option selected:", selected);
+            switch (selected) {
+                case "close app": {
+                    running = false;
+                }
+                default: { Utilities.dialog("invalid option: " + selected); }
+            }
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("iniciando...");
-        JOptionPane.showMessageDialog(null, "hello world");
+        Main main = new Main();
+        main.mainMenu();
     }
 }
