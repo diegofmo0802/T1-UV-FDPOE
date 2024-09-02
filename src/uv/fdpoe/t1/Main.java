b344bbaaabcf1a53158b90ea54d3aa8ef27eec60
diegofmo0802 <diegofmo0802@gmail.com>
2024-09-01 19:23:43 -0500

    first commit

---
+++ src/uv/fdpoe/t1/Main.java
@@ -0,0 +1,11 @@
+package uv.fdpoe.t1;
+import javax.swing.JOptionPane;
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("iniciando...");
+        JOptionPane.showMessageDialog(null, "hello world");
+    }
+}
