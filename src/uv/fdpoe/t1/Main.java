bc5541b0342e397546f97699a15a63b3d5995738
diegofmo0802 <diegofmo0802@gmail.com>
2024-09-01 21:24:05 -0500

    main menu anded about us

--- src/uv/fdpoe/t1/Main.java
+++ src/uv/fdpoe/t1/Main.java
@@ -15,12 +15,21 @@ public class Main {
             String selected = Utilities.selectWindow("Main menu", "select an option", options);
             Utilities.logConcat("option selected:", selected);
             switch (selected) {
-                case "close app": {
-                    running = false;
+                case "about us" -> {
+                    aboutUs();
                 }
-                default: { Utilities.dialog("invalid option: " + selected); }
+                case "close app" ->  { running = false; }
+                default -> { Utilities.dialog("invalid option: " + selected); }
             }
         }
+    }
+    public void aboutUs() {
+        String message = Utilities.concat("\n",
+            "Diego Fernando Moreno     - 202363205",
+            "Veruzka Katriana Guapache - 202378085",
+            "Oscar David Cadavid       - 202363228"
+        );
+        Utilities.dialog(message);
     }
     /**
      * @param args the command line arguments
