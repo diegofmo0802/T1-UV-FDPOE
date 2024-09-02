66924e32887f35252b746be582d3a0d22789ffd8
diegofmo0802 <diegofmo0802@gmail.com>
2024-09-01 23:27:45 -0500

    se añadio el menu de gestion de entity

--- src/uv/fdpoe/t1/Main.java
+++ src/uv/fdpoe/t1/Main.java
@@ -1,7 +1,7 @@
 package uv.fdpoe.t1;
-import java.util.ArrayList;
+
 public class Main {
-    public ArrayList<People> peoples;
+    public PeopleManager peoples = new PeopleManager();
     public void mainMenu() {
         boolean running = true;
         String[] options = {
@@ -15,18 +15,47 @@ public class Main {
             String selected = Utilities.selectWindow("Main menu", "select an option", options);
             Utilities.logConcat("option selected:", selected);
             switch (selected) {
-                case "about us" -> {
-                    aboutUs();
+                case  "entity managment" -> {
+                    entityMenu();
                 }
-                case "close app" ->  { running = false; }
+                case "list menu" -> { listMenu(); }
+                case "about us"  -> { aboutUs(); }
+                case "close app" -> { running = false; }
                 default -> { Utilities.dialog("invalid option: " + selected); }
             }
         }
     }
+    public void entityMenu() {
+        String[] options = {
+            "add",
+            "delete",
+            "modify",
+        };
+        String selected = Utilities.selectWindow("entity menu", "select an option", options);
+        switch(selected) {
+            case "add" -> { peoples.addPeople(); }
+            case "delete" -> { peoples.delPeople(); }
+            case "modify" -> { peoples.editPeople(); }
+            default -> { Utilities.dialog("invalid option: " + selected); }
+
+        }
+    }
+    public void listMenu() {
+        String[] options = {
+            "general list",
+            "query by id",
+            "query by age",
+        };
+        String selected = Utilities.selectWindow("list menu", "select an option", options);
+        switch (selected) {
+                case "list menu" -> { listMenu(); }
+                default -> { Utilities.dialog("invalid option: " + selected); }
+            }
+    }
     public void aboutUs() {
         String message = Utilities.concat("\n",
             "Diego Fernando Moreno     - 202363205",
-            "Veruzka Katriana Guapache - 202378085",
+            "Veruzka Katriana Guapacha - 202378085",
             "Oscar David Cadavid       - 202363228"
         );
         Utilities.dialog(message);
